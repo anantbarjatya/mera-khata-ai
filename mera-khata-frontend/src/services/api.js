ebb789633e03cb5df1923baa9552507468fdc93c
anantbarjatya <barjatyaanant23@gmail.com>
2026-06-11 21:34:52 +0530

    Debug VITE_API_URL

--- mera-khata-frontend/src/services/api.js
+++ mera-khata-frontend/src/services/api.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+console.log("API URL =", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL || "http://localhost:3001/api",
